1ff5e9ef41da7fb5197fa3dc164437c4c916bbe1
Subbhashit Mukherjee <43717493+23subbhashit@users.noreply.github.com>
2020-04-25 21:22:03 +0530

    Update

--- A plane journey.py
+++ A plane journey.py
@@ -1,27 +1,22 @@
-n,m=map(int,input().split())
-l=[int(x) for x in input().split()]
-c=[0 for i in range(len(l))]
-c2=[1 for i in range(len(l))]
-l.sort(reverse=True)
-l1=[int(x) for x in input().split()]
-c1=[0 for i in range(len(l1))]
-l1.sort(reverse=True)
-count=0
-if l[0]>l1[0]:
-    print(-1)
-else:
-    while c!=c2:
-        for i in range(len(l1)):
-            for j in range(len(l)):
-                if l1[i]>=l[j]:
-                    if c1[i]!=1 and c[j]!=1:
-                        c1[i]+=1
-                        c[j]+=1
-        
-        c1=[0 for i in range(len(l1))]
-        count=count+2
-    print(count-1)
-
-     
-
-    
+from sys import stdin,stdout
+import bisect
+#a=[10,11,20]
+#print(bisect.bisect_right(a,12))
+for _ in range(1):
+	#n,shift,ch=stdin.readline().split()
+	m,n=map(int,stdin.readline().split())
+	gp=sorted(map(int,stdin.readline().split()))
+	a=sorted(map(int,stdin.readline().split()))
+	if gp[-1]>a[-1]:stdout.write('-1\n')
+	else:
+		ind=m;pos=[]
+		for i in range(n-1,-1,-1):
+			ind=bisect.bisect_right(gp[:ind],a[i])-1
+			if ind==-1:break
+			pos+=[ind]
+		ans=0
+		#print(pos)
+		for v,v1 in zip(pos,pos[1:]):
+			ans=max(ans,v-v1)
+		ans=max(ans,pos[-1]+1)
+		print(ans*2-1)
